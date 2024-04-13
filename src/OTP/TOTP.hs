a57ed0646d05b88b5aab668d6c95290a92e3daea
Hécate Moonlight <hecate+github@glitchbra.in>
2024-04-13 20:51:18 +0200

    Bit of a fresh-up

--- src/OTP/TOTP.hs
+++ src/OTP/TOTP.hs
@@ -27,7 +27,7 @@ module OTP.TOTP
   , totpToURI
   ) where
 
-import Chronos (Time (..), Timespan (..))
+import Chronos (Time (..), Timespan (..), asSeconds)
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.Display (display)
@@ -40,7 +40,6 @@ import OTP.Commons
   ( Algorithm
   , Digits
   , OTP
-  , asSeconds
   , totpCounter
   , totpCounterRange
   )
@@ -52,6 +51,48 @@ import OTP.HOTP
   , newSHA256Key
   , newSHA512Key
   )
+
+-- $usage
+--
+-- > import Chronos (Timespan, now, second)
+-- > import Data.ByteString.Base32 qualified as Base32
+-- > import Data.Maybe (fromJust)
+-- > import Data.Text (Text)
+-- > import OTP.Commons
+-- > import OTP.TOTP
+-- > import Sel.HMAC.SHA256 qualified as HMAC
+-- > import Torsor (scale)
+-- >
+-- > period :: Timespan
+-- > period = scale 30 second
+-- >
+-- > sixDigits :: Digits
+-- > sixDigits = fromJust $ mkDigits 6
+-- >
+-- > uriFromKey :: Text -> Text -> HMAC.AuthenticationKey -> Text
+-- > uriFromKey domain email key =
+-- >  let
+-- >    issuer = "your-domain"
+-- >   in
+-- >    totpToURI
+-- >      (Base32.encodeBase32Unpadded $ HMAC.unsafeAuthenticationKeyToBinary key)
+-- >      email
+-- >      issuer
+-- >      sixDigits
+-- >      period
+-- >      HMAC_SHA1
+-- >
+-- > validateTOTP :: HMAC.AuthenticationKey -> Text -> IO Bool
+-- > validateTOTP key code = do
+-- >  timestamp <- now
+-- >  pure $
+-- >    totpSHA1Check
+-- >      key
+-- >      (1, 1)
+-- >      timestamp
+-- >      period
+-- >      sixDigits
+-- >      code
 
 -- | Compute a Time-based One-Time Password using secret key and time.
 --
